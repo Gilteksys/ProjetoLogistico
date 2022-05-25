df950b13078e479dc796c0a3c79c88902f855829
Gil C S <gilteksys21@gmail.com>
2022-05-25 16:39:58 -0300

    Criacao de Tabelas

---
+++ logap/src/main/java/com/projeto/log/logap/LogapApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.log.logap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogapApplication.class, args);
+	}
+
+}
---
+++ logap/src/main/resources/db/migration/V002__cria-tabela-clientes.sql
@@ -0,0 +1,10 @@
+create table clientes(
+id bigint not null auto_increment,
+nome varchar(60) not null,
+email varchar (100) not null,
+telefone varchar(15) not null,
+
+primary key (id)
+
+
+);
