7a28f86b380cdc8f436c936eff9075a44091b4d5
Gil C S <teksyseletronica@terra.com.br>
2022-05-30 01:05:10 -0300

    Ocorrencias

--- logap/src/main/java/com/projeto/log/logap/domain/model/Entrega.java
+++ logap/src/main/java/com/projeto/log/logap/domain/model/Entrega.java
@@ -2,6 +2,8 @@ package com.projeto.log.logap.domain.model;
 
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Embedded;
 import javax.persistence.Entity;
@@ -11,6 +13,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -28,17 +31,35 @@ public class Entrega {
 	private Long id;	
 	
 	@ManyToOne
-	private Clientes clientes;
+	private Clientes clientes;	
 	
 	@Embedded
-	private Destinatario destinatario;
+	private Destinatario destinatario;	
 	
 	private BigDecimal taxa; 
+	
+	@OneToMany(mappedBy = "entrega")
+	private List<Ocorrencia> ocorrencias = new ArrayList<>();
 	
 	@Enumerated(EnumType.STRING)	
 	private StatusEntrega status;
 	
 	private OffsetDateTime dataPedido;	
 	
-	private OffsetDateTime dataFinalizacao;	
+	private OffsetDateTime dataFinalizacao;
+
+	public Ocorrencia adicionarOcorrencia(String descricao) {
+		Ocorrencia ocorrencia = new Ocorrencia();
+		ocorrencia.setDescricao(descricao);
+		ocorrencia.setDataRegistro(OffsetDateTime.now());
+		ocorrencia.setEntrega(this);
+		
+		this.getOcorrencias().add(ocorrencia);
+		
+		return ocorrencia;
+		
+		
+		
+				
+	}
 }
